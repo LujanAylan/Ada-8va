f0e746a9b8e5a3efc203e575db91066edc63c1ae
LujanAylan <aldanaylan@gmail.com>
2019-08-22 10:57:12 -0300

    Cambios

--- Ejercicio04.java
+++ Ejercicio04.java
@@ -1,11 +1,32 @@
-import java.util.*;
+import java.util.Scanner;
 
 /**
  * Ejercicio04
  */
 public class Ejercicio04 {
 
-    public static Scanner teclado = new Scanner (System.in);
-        public static void main(String []args) {
-         }
+    public static Scanner Teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int valor;
+        int valorMax = 0;
+        int posicion = 0;
+
+        System.out.println("Ingrese el valor");
+        valor = Teclado.nextInt();
+
+        for (int i = 1; i <= 15; i++) {
+
+            if (valor > valorMax) {
+                valorMax = valor;
+                posicion = i;
+
+            } else {
+
+            }
+            System.out.println("El valor maximo es " + valorMax);
+            System.out.println("Su posicion es " + posicion);
+        }
+    }
 }
--- Ejercicio05.java
+++ Ejercicio05.java
@@ -1,8 +1,52 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Ejercicio05{
-    public static Scanner teclado = new Scanner (System.in);
-        public static void main(String []args) {
-         }
+/**
+ * Ejercicio05
+ */
+public class Ejercicio05 {
 
+    public static Scanner Teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int a = 0, b = 0, c = 0;
+        double t1 = 0, t2 = 0, t3 = 0, tt = 0;
+        int n;
+        double cn = 1;
+        double v;
+
+        System.out.println("Ingrese el nro de ventas");
+        n = Teclado.nextInt();
+
+        while (cn <= n) {
+            System.out.println("Ingrese el monto de la venta");
+            v = Teclado.nextDouble();
+
+            if (v > 1000) {
+                a = a + 1;
+                t1 = t1 + v;
+
+            } else {
+                if (v > 500) {
+                    b = b + 1;
+                    t2 = t2 + v;
+
+                } else {
+                    c = c + 1;
+                    t3 = t3 + v;
+                }
+
+            }
+            tt = tt + v;
+            cn = cn + 1;
+
+        }
+
+        System.out.println("Cantidad de ventas y el total de ventas mayores a 1000 es: " + a + " , " + t1);
+        System.out.println("Cantidad de ventas y el total de ventas mayores a 500  pero menores o iguales a 1000 es: "
+                + b + " , " + t2);
+        System.out.println("Cantidad de ventas y el total de ventas menores o iguales a 500 es: " + c + " , " + t3);
+        System.out.println("El total de ventas es: " + tt);
+
+    }
 }
